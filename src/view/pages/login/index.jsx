c4795764f5da53a17260f56260a64377fac4e3ec
Iwaymen <iwaymen@schrodingernft.ai>
2024-04-03 14:54:29 +0800

    Code update

--- src/view/pages/login/index.jsx
+++ src/view/pages/login/index.jsx
@@ -6,50 +6,78 @@
  * 
  */
 // GENERIC IMPORT
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Box} from '@mui/material';
-import { GoogleLogin, googleLogout, useGoogleLogin } from '@react-oauth/google';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 
 // OTHER COMPONENT, HOOK, CONTEXT
 
-// STYLE IMPORT
-import useStyles from './styles';
+import axios from 'axios';
 
-const LoginPage = () => {
-    // DECLARE STYLE
-    const classes = useStyles();
-    const [state, setState] = useState();
+function LoginPage() {
 
-    const responseGoogle = (response) => {
-        console.log(response); // You can see the response object in the console
-        const jwtToken = response.tokenId; // Extract the JWT token from the response
-        // You can now use the jwtToken as needed, such as sending it to your server
-    };
+    const getUserStorageData = () => {
+        const useData = localStorage.getItem("useData");
+        return JSON.parse(useData) || [];
+    }
+    const getProfileStorageData = () => {
+        const profileData = localStorage.getItem("profileData");
+        return JSON.parse(profileData) || [];
+    }
+    const [ user, setUser ] = useState(getUserStorageData());
+    const [ profile, setProfile ] = useState(getProfileStorageData());
 
-    const responseMessage = (response) => {
-        if (response?.credential) {
-            const jwtToken = response?.credential;
-            setState(jwtToken);
-            console.log("jwtToken: ", jwtToken);
-            localStorage.setItem("jwtToken", jwtToken);
-        }
-        console.log("Output: ", response);
-    };
+    const login = useGoogleLogin({
+        onSuccess: (codeResponse) => {
+            localStorage.setItem("access_token", codeResponse.access_token);
+            localStorage.setItem("useData", JSON.stringify(codeResponse));
+            setUser(codeResponse)
+        },
+        onError: (error) => console.log('Login Failed:', error)
+    });
 
-    const errorMessage = (error) => {
-        console.log("Output: ", error);
-    };
+    useEffect(
+        () => {
+            if (user) {
+                axios
+                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                        headers: {
+                            Authorization: `Bearer ${user.access_token}`,
+                            Accept: 'application/json'
+                        }
+                    })
+                    .then((res) => {
+                        setProfile(res.data);
+                        localStorage.setItem("profileData", JSON.stringify(res.data));
+                    })
+                    .catch((err) => console.log(err));
+            }
+        },
+        [ user ]
+    );
 
+    // log out function to log the user out of google and set the profile array to null
     const logOut = () => {
         googleLogout();
+        setProfile(null);
+        localStorage.clear();
     };
 
-    return (   
-        <Box className={classes.container}>
-            Token: {state}
-            {state ? `Logged in already ${<button onClick={logOut}>Logout</button>}` : <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />}
+    return (
+        <Box>
+            {profile ? (
+                <Box>
+                    <Box mb={1}><strong>User Logged in</strong></Box>
+                    <Box mb={1}><strong>Name:</strong> {profile.name}</Box>
+                    <Box mb={1}><strong>Email Address:</strong> {profile.email}</Box>
+                    <Box mb={1}><strong>Access token:</strong> {user.access_token}</Box>
+                    <button onClick={logOut}>Log out</button>
+                </Box>
+            ) : (
+                <button onClick={login}>Sign in with Google</button>
+            )}
         </Box>
-    )
+    );
 }
 
 export default LoginPage;
