e4ea812a6d90cb26aa8053672cf6f4d2492fc8b3
Iwaymen <iwaymen@schrodingernft.ai>
2024-04-03 14:38:26 +0800

    Code update

--- src/view/pages/login/index.jsx
+++ src/view/pages/login/index.jsx
@@ -6,9 +6,9 @@
  * 
  */
 // GENERIC IMPORT
-import React from 'react';
+import React, {useState} from 'react';
 import {Box} from '@mui/material';
-import { GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin, googleLogout, useGoogleLogin } from '@react-oauth/google';
 
 // OTHER COMPONENT, HOOK, CONTEXT
 
@@ -18,6 +18,7 @@ import useStyles from './styles';
 const LoginPage = () => {
     // DECLARE STYLE
     const classes = useStyles();
+    const [state, setState] = useState();
 
     const responseGoogle = (response) => {
         console.log(response); // You can see the response object in the console
@@ -28,18 +29,25 @@ const LoginPage = () => {
     const responseMessage = (response) => {
         if (response?.credential) {
             const jwtToken = response?.credential;
+            setState(jwtToken);
             console.log("jwtToken: ", jwtToken);
             localStorage.setItem("jwtToken", jwtToken);
         }
         console.log("Output: ", response);
     };
+
     const errorMessage = (error) => {
         console.log("Output: ", error);
     };
 
+    const logOut = () => {
+        googleLogout();
+    };
+
     return (   
         <Box className={classes.container}>
-            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+            Token: {state}
+            {state ? `Logged in already ${<button onClick={logOut}>Logout</button>}` : <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />}
         </Box>
     )
 }
